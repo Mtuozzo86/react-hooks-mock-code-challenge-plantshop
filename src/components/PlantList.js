be154962a57cfa46acc8c0407e928bc4537a6adb
Mtuozzo86 <Mtuozzo86@gmail.com>
2021-09-26 17:41:33 -0400

    core deliverables

--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -1,10 +1,20 @@
 import React from "react";
 import PlantCard from "./PlantCard";
 
-function PlantList() {
-  return (
-    <ul className="cards">{/* render PlantCards components in here */}</ul>
-  );
+function PlantList({ plants }) {
+  const displayPlants = plants.map((item) => {
+    return (
+      <PlantCard
+        key={item.id}
+        id={item.id}
+        name={item.name}
+        image={item.image}
+        price={item.price}
+      />
+    );
+  });
+
+  return <ul className="cards">{displayPlants}</ul>;
 }
 
 export default PlantList;
--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+  useEffect(() => {
+    fetch("http://localhost:6001/plants")
+      .then((resp) => resp.json())
+      .then((data) => setPlants(data));
+  }, []);
+
+  const [plants, setPlants] = useState([]);
+  const [searched, setSearched] = useState("");
+
+  function handleForm(formData) {
+    fetch("http://localhost:6001/plants", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    }).then(() => {
+      const setNewPlant = [...plants, formData];
+      setPlants(setNewPlant);
+    });
+  }
+
+  const filtered = plants.filter((item) => {
+    return item.name.toLowerCase().includes(searched.toLowerCase());
+  });
+
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm handleForm={handleForm} />
+      <Search handleSearch={setSearched} />
+      <PlantList plants={filtered} />
     </main>
   );
 }
